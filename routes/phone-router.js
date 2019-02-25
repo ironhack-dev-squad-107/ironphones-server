9c65f65fe3cd8d7c56d171c90c18cefb1fb83a8d
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2019-02-25 17:47:30 +0100

    POST phones

--- routes/phone-router.js
+++ routes/phone-router.js
@@ -16,6 +16,15 @@ router.get("/phones", (req, res, next) => {
 router.get("/phones/:phoneId", (req, res, next) => {
   const { phoneId } = req.params;
   Phone.findById(phoneId)
+    // send the DB query result document as a JSON response to the client
+    .then(phoneDoc => res.json(phoneDoc))
+    .catch(err => next(err));
+});
+
+router.post("/phones", (req, res, next) => {
+  const { phoneModel, brand, price, image, specs } = req.body;
+  Phone.create({ phoneModel, brand, price, image, specs })
+    // send the DB query result document as a JSON response to the client
     .then(phoneDoc => res.json(phoneDoc))
     .catch(err => next(err));
 });
