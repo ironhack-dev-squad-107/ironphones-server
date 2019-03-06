27bc6e9981317509198d86ebc4bdb9140bed65d2
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2019-03-06 18:25:48 +0100

    Add React production files

---
+++ public/precache-manifest.429fa991ac8123a67c4c708f512890ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "541526ba250deddc7b17",
+    "url": "/static/js/main.541526ba.chunk.js"
+  },
+  {
+    "revision": "b26f420519fd58100ecb",
+    "url": "/static/js/2.b26f4205.chunk.js"
+  },
+  {
+    "revision": "541526ba250deddc7b17",
+    "url": "/static/css/main.a4f2bc29.chunk.css"
+  },
+  {
+    "revision": "e02b0a0c62f5151d5cd8ab1775f8764b",
+    "url": "/index.html"
+  }
+];
